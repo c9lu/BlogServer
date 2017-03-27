bf34cd5554c3fe7d6268302073e37d09c553c5c4
Chen Lu <chenlucircle@gmial.com>
2017-03-26 22:03:04 -0400

    added mongodb api

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var http = require('http')
 
 var app = express();
 
+var mongoapi = require('./mongo.api.js');
+
 app.configure(function(){
   app.set('port', process.env.PORT || 4000);
 
@@ -20,7 +22,29 @@ app.get('/hi', function(req, res) {
 // res.render('index.html')
   res.send('{"a": "Hello World"}')
 });
+app.get('/', function(req, res){
 
+  mongoapi.getAllPosts().then
+  (
+    function (result){
+      
+      res.send(JSON.stringify(result));
+
+    } 
+  ); 
+});
+
+app.get('/Posts/:id', function(request, response){
+  console.log("[app.get posts] " + request.params.id);
+    mongoapi.getPostById(request.params.id).then(
+      function(result){
+        console.log(JSON.stringify(result));
+        response.send(JSON.stringify(result));
+      }
+
+    );
+}
+);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
